5101a300f888a4e10b1a08f90663e0301fbf0709
Rachel <rmoshier@gmail.com>
2014-10-08 15:13:16 -0700

    added a session to create method in home controller

--- farmar-rails/app/controllers/home_controller.rb
+++ farmar-rails/app/controllers/home_controller.rb
@@ -9,11 +9,16 @@ class HomeController < ApplicationController
   def create
     @vendor = Vendor.new(params.require(:vendor).permit(:name))
     @vendor.save
+    session[:v_id] = @vendor.id
+    # to log-out set session v_id to nil
+    # maybe do a conditional here?
+
     redirect_to "/home/vendor_landing"
     # redirect_to root_path
   end
 
   def vendor_landing
+    @v = Vendor.find(session[:v_id])
   end
 
 end
