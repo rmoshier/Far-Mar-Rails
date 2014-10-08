4125dc3784488f063cb6d1665cc7845404d04400
Crystal Perreira <cvperreira@gmail.com>
2014-10-07 17:04:01 -0700

    Start form

--- farmar-rails/app/controllers/home_controller.rb
+++ farmar-rails/app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def new_vendor
+  end
+
 end
--- farmar-rails/config/routes.rb
+++ farmar-rails/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
   get "/", to: "home#index"
+  get "home/new_vendor", to: "home#new_vendor"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
